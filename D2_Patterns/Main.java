85d436550d4ebe4ae5c967b34f7b0468234ff646
Shubham Dubey <shubhamdubey05092001@gmail.com>
2025-10-07 20:01:11 +0530

    New

--- D2_Patterns/Main.java
+++ D2_Patterns/Main.java
@@ -618,8 +618,192 @@ public static void Pattern34(int n){
     }
 }
 
+public static void Pattern35(int n){
+    for(int i =1; i<=2*n;i++){
+        if(i<=n){
+                // spaces
+           for(int j = 1; j<=i-1; j++){
+            System.out.print(" ");
+           }
+        // star
+        for(int j =1; j<=n-i+1;j++){
+            System.out.print("* ");
+             }
+        }
+        else{
+            //spaces
+            for(int j =1; j<=n-(i-n); j++){
+                System.out.print(" ");
+            }
+            // star
+            for(int j=1; j<=i-n; j++){
+                System.out.print("* ");
+            }
+        }
+      
+        System.out.println();
+    }
+}
+
+public static void Pattern36(int n){
+    for(int i = 1; i<=n; i++){
+        if(i == n){
+            for(int j =1; j<=2*n-1; j++){
+                System.out.print("*");
+            }
+        } else{
+             for(int j =1; j<=2*n-1; j++){
+            if(j<n){
+                if((i+j)==n+1){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            else{
+                  if((j-i)==n-1){
+                    System.out.print("*");
+                  }
+                  else{
+                    System.out.print(" ");
+                  }
+            }
+        }
+        }
+        
+          System.out.println();
+    }
+}
+
+public static void Pattern37(int n){
+    for(int i = 1; i<=n; i++){
+         if(i==1){
+            for(int j=1; j<=2*n-1; j++){
+                System.out.print("*");
+            }
+         }else{
+            for(int j = 1; j<=2*n-1;j++){
+                if(j<=n){
+                    if(i==j){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                else{
+                    if((i+j)==2*n){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+            }
+         }
+         
+         System.out.println();
+        }
+}
+
+public static void Pattern38(int n){
+    for(int i=1; i<=2*n-1; i++){
+        if(i<=n){
+            for(int j =1; j<=2*n-1; j++){
+                  
+                if(j<=n){
+                    if(i+j == n+1){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }else{
+                     if(j-i == n-1){
+                        System.out.print("*");
+                     }
+                     else{
+                        System.out.print(" ");
+                     }
+                }
+            } 
+
+        }
+        else{
+            for(int j =1; j<=2*n-1; j++){
+                  if(j<=n){
+                    if(i-j == n-1){
+                        System.out.print("*");
+                    }
+                    else {
+                        System.out.print(" ");
+                    }
+                  }
+                  else {
+                    if(i+j == 2*n+(n-1)){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                  }
+            }
+        }
+    
+         System.out.println();
+    }
+}
+
+public static void Pattern39(int n){
+    for(int i = 1; i<=n; i++){
+        int val=1;
+        for(int j=1; j<=i; j++){
+            System.out.print(val+" ");
+            val+=2;
+        }
+        System.out.println();
+    }
+}
+
+public static void Pattern40(int n){
+    for(int i=1; i<=n; i++){
+        if(i%2 == 0){
+            char ch = 'A';
+            for(int j =1; j<=i; j++){
+                System.out.print(ch++ +" ");
+            }
+        }else{
+            for(int j=1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+        }
+        System.out.println();
+    }
+}
+
+public static void Pattern41(int n){
+    for(int i = 1; i<=n; i++){
+        for(int j =1; j<=n; j++){
+            if(i==j || i+j == n+1){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+}
+
+public static void Pattern42(int n){
+    for(int i=1; i<=n;  i++){
+        
+    }
+}
+
 public static void main(String[] args) {
-    Pattern34(5);
+    Pattern41(7);
   
 }
 }
