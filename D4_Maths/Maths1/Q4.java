c39276c1124113b83b74d5b9474dca417bf6119b
Shubham Dubey <shubhamdubey05092001@gmail.com>
2025-10-12 21:27:32 +0530

    2s

--- D4_Maths/Maths1/Q4.java
+++ D4_Maths/Maths1/Q4.java
@@ -2,12 +2,43 @@ package D4_Maths.Maths1;
 
 public class Q4 {
   public static void main(String[] args) {
-    
+    System.out.println(twosComplement("1011"));
   }
 
+
+  public static String reverse(String s){
+    String res = "";
+    int len  = s.length();
+     for(int i = len-1; i>=0; i--){
+      res+=s.charAt(i);
+     }
+    return res;
+
+  }
   public static String twosComplement(String s){
         String temp = onesComplement(s);
-        
+         String comple = addOne(temp);
+         return reverse(comple);
+  }
+  public static String addOne(String s){
+         char carry = '1';
+         String temp = "";
+         for(int i = s.length()-1; i>=0; i--){
+             char c = s.charAt(i);
+             if(c== '1' && carry == '1'){
+              temp +='0';
+              carry = '1';
+             }
+             else if(carry=='1' && c=='0'){
+              temp  += '1';
+              carry = '0';       }
+              else {
+                temp +='c';
+              }
+         }
+         return temp;
+         
+
   }
    public static String onesComplement(String s){
     String ans = "";
